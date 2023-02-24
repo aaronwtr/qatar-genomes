8768ced9cf13674b241e42c700795b09060c4f74
Aaron Wenteler <aaronwenteler@gmail.com>
2023-02-24 18:28:11 +0000

    read in manually curated dataset for testing purposes

--- src/phewas/phewas.R
+++ src/phewas/phewas.R
@@ -1,12 +1,14 @@
 # Installs -----------------------------------------------------------------
 install.packages("devtools")
 install.packages(c("dplyr", "tidyr", "ggplot2", "MASS", "meta", "ggrepel", "DT"))
+install.packages("readxl")
 devtools::install_github("PheWAS/PheWAS")
 library(PheWAS)
+library(readxl)
+
 
 # Dataloading -------------------------------------------------------------
 setwd("~/Desktop/PhD/Research/QMUL/Research/Qatar Genomes Project/qatar-genomes")
-data <- read.csv("data/phewas_tables/TRPV1_phewas_table.csv")
+data <- read_excel("data/phewas_tables/TRPV1_phewas_table.xlsx", sheet = 2)
 phenotype <- data[, c(5:ncol(data))]
 phenotype <- createPhenotypes(phenotype)
-
