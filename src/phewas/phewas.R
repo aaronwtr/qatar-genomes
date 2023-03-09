c99600fe6ffb2dbf13ab6ab4524a1fc84b2abe38
Aaron Wenteler <aaronwenteler@gmail.com>
2023-03-09 16:32:54 +0000

    fix count column bug in createPhenotypes

--- src/phewas/phewas.R
+++ src/phewas/phewas.R
@@ -1,14 +1,41 @@
-# Installs -----------------------------------------------------------------
-install.packages("devtools")
-install.packages(c("dplyr", "tidyr", "ggplot2", "MASS", "meta", "ggrepel", "DT"))
-install.packages("readxl")
-devtools::install_github("PheWAS/PheWAS")
 library(PheWAS)
+library(data.table)
+library(tidyverse)
 library(readxl)
 
 
-# Dataloading -------------------------------------------------------------
 setwd("~/Desktop/PhD/Research/QMUL/Research/Qatar Genomes Project/qatar-genomes")
-data <- read_excel("data/phewas_tables/TRPV1_phewas_table.xlsx", sheet = 2)
-phenotype <- data[, c(5:ncol(data))]
-phenotype <- createPhenotypes(phenotype)
+#(data <- read_excel("C:/Enserio/OneDrive - Queen Mary, University of London/Projects/AWenteler/TRPV1_phewas_table.xlsx",sheet = "Sheet1"))
+
+data <- fread("data/phewas_tables/TRPV1_phewas_table.csv")
+
+# Create sex table
+id.sex <- data[,c("patient_id","gender")]
+id.sex$gender[which(id.sex$gender=="MALE")]<-"M"
+id.sex$gender[which(id.sex$gender=="FEMALE")]<-"F"
+
+data$patient_id <- as.character(data$patient_id)
+data$gender <- as.factor(data$gender)
+id.sex$patient_id <- as.character(id.sex$patient_id)
+id.sex$gender <- as.factor(id.sex$gender)
+
+# Read pheno test table
+phenodata <- fread("data/phewas_tables/TRPV1_icd10_test.csv")
+
+# Create Sex table matching the test phenotype table (test subset)
+id.sex.test <- id.sex[which(id.sex$patient_id %in% as.character(phenodata$id)),]
+head(id.sex.test)
+
+colnames(phenodata)
+colnames(id.sex)
+
+colnames(phenodata)[1] <- "patient_id"
+
+# Map phenotypes
+
+# TODO Add full.population.ids argument
+phenotyes <- createPhenotypes(phenodata, min.code.count = 1, id.sex = id.sex.test,
+                              vocabulary.map = PheWAS::phecode_map_icd10)
+phenotyes[1:10,]
+
+
