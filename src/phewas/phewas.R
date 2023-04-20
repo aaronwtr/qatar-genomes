ed9816f3441b5ecbb7792bbaf3202ebec80a0797
Aaron <aaronwenteler@gmail.com>
2023-04-20 12:23:33 +0100

    Adjust phewas script for BMI data

--- src/phewas/phewas.R
+++ src/phewas/phewas.R
@@ -8,7 +8,7 @@ library(progress)
 
 setwd("~/Desktop/PhD/Research/QMUL/Research/Qatar Genomes Project/qatar-genomes")
 
-genodata <- fread("data/phewas_input/gene_data.csv")
+genodata <- fread("data/phewas_input/gene_data_bmi.csv")
 phenodata <- fread("data/phewas_input/phenotype_data.csv")
 
 ## Processing phenotype data
@@ -41,16 +41,16 @@ phecodes <- addPhecodeInfo(phecodes)
 
 ## Processing auxiliary phewas data and genotype of interest
 
-gene_cols <- colnames(genodata)[5:ncol(genodata)]
+gene_cols <- colnames(genodata)[6:ncol(genodata)]
 genodata <- data.frame(genodata)
-
 
 pb <- progress_bar$new(total = length(gene_cols), format = "[:bar] :percent :eta")
 
 phe_results <- list()
 
-covariates <- cbind(id.sex, genodata[, c("age")])
+covariates <- cbind(id.sex, genodata[, c("age")], genodata[, c("BMI")])
 colnames(covariates)[3] <- "age"
+colnames(covariates)[4] <- "BMI"
 covariates$patient_id <- as.character(covariates$patient_id)
 
 # all_data = inner_join(inner_join(covariates, genotype),phenotypes)
@@ -61,25 +61,28 @@ covariates$patient_id <- as.character(covariates$patient_id)
 for (gene in gene_cols) {
   pb$tick()
   gene <- gsub("-", ".", gene)
+  gene <- gsub(":", ".", gene)
+  print(gene)
   genotype <- genodata[, c("patient_id", gene)]
 
   joined_genos <- inner_join(phenotypes_nonan, genotype, by = "patient_id")
   
-  covariates <- cbind(id.sex, genodata[, c("age")])
+  covariates <- cbind(id.sex, genodata[, c("age")], genodata[, c("BMI")])
   #covariates$patient_id <- as.numeric(as.character(covariates$patient_id))
   covariates.filtered <- filter(covariates, patient_id %in% joined_genos$patient_id)
   
   cov_gen_data = inner_join(inner_join(covariates.filtered, genotype),
                             phenotypes_nonan)
   colnames(cov_gen_data)[3] <- "age"
+  colnames(cov_gen_data)[4] <- "BMI"
   cov_gen_data$patient_id <- as.character(cov_gen_data$patient_id)
   
   
   # cov_gen_data$gender <- as.factor(id.sex$gender)
   
   results <- phewas(phenotypes=names(phenotypes_nonan)[-1], genotypes=c(gene), 
-                    covariates=c("age", "gender"), data=cov_gen_data, cores=4, 
-                    significance.threshold = c("fdr"))
+                    covariates=c("age", "gender", "BMI"), data=cov_gen_data, 
+                    cores=4, significance.threshold = c("fdr"))
   
   
   phe_results[[gene]] <- results
