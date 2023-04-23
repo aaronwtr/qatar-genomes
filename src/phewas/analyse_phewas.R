9dd15874317a0788e41d33c4749cbb01ea3312f0
Aaron <aaronwenteler@gmail.com>
2023-04-23 15:09:05 +0100

    processing phewas_bmi results

--- src/phewas/analyse_phewas.R
+++ src/phewas/analyse_phewas.R
@@ -3,11 +3,17 @@ library(tidyverse)
 library(dplyr)
 library(PheWAS)
 
-setwd("~/Desktop/PhD/Research/QMUL/Research/Qatar Genomes Project/qatar-genomes")
+setwd("~/Desktop/PhD/Research/QMUL/Research/qatar-genomes")
 
 phewas_list <- readRDS("outputs/phe_results_v1_2.rds")
+phewas_list_bmi <- readRDS("outputs/phe_results_v1_bmi.rds")
 
 combined_phewas <- bind_rows(phewas_list)
 combined_phewas <- setDT(combined_phewas)
 combined_phewas <- combined_phewas[combined_phewas$p < 0.05,]
 combined_phewas <- addPhecodeInfo(combined_phewas)
+
+combined_phewas_bmi <- bind_rows(phewas_list)
+combined_phewas_bmi <- setDT(combined_phewas)
+combined_phewas_bmi <- combined_phewas[combined_phewas$p < 0.05,]
+combined_phewas_bmi <- addPhecodeInfo(combined_phewas)
